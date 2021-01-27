7d11b325069a19c29fc717b8591d3e2ed0a60dca
Matthew <matthewcrews@gmail.com>
2021-01-26 18:43:38 -0800

    spelling

--- 2021-01-22-MachineAllocation/2021-01-28-MachineAllocationPercentConstraints.fsx
+++ 2021-01-22-MachineAllocation/2021-01-28-MachineAllocationPercentConstraints.fsx
@@ -1,97 +1,105 @@
 fsi.ShowDeclarationValues <- false
-
-
-// The Domain
-[<RequireQualifiedAccess>]
-type JobType =
-    | A
-    | B
-    | C
-
-type Job = {
-    Id : int
-    JobType : JobType
-    Size : float
-} with
-    override this.ToString () =
-        $"Job_{this.Id}"
-
-type Machine = {
-    Id : int
-    JobTypes : Set<JobType>
-} with
-    override this.ToString () =
-        $"Machine_{this.Id}"
-
-
-// Set of JobTypes for iterating over and sampling from
-let jobTypes = 
-    [|
-        JobType.A
-        JobType.B
-        JobType.C
-    |]
-
-// Some theoretical JobTypeSets to be used in generating
-// random Machines
-let jobTypeSets =
-    [|
-        Set jobTypes
-        Set jobTypes.[1..]
-        Set jobTypes.[..1]
-    |]
-
-// Setting up parameters for the example
-let rng = System.Random(123)
-let numberOfJobs = 20
-let numberOfMachines = 5
-let minJobSize = 1
-let maxJobSize = 3
-let maxWorkDifference = 2.0
-
-let randomJobSize (rng: System.Random) =
-    rng.Next(minJobSize, maxJobSize)
-    |> float
-
-let randomJobType (rng: System.Random) =
-    jobTypes.[rng.Next(0, jobTypes.Length)]
-
-let randomJobTypeSet (rng: System.Random) =
-    jobTypeSets.[rng.Next(0, jobTypeSets.Length)]
-
-module Map =
-
-    // Useful when you want to look up a key in a Map but you want it to provide
-    // a default value if the key is missing
-    let tryFindDefault (key: 'a) (defaultValue: 'v) (m: Map<'a, 'v>) =
-        match Map.tryFind key m with
-        | Some v -> v
-        | None -> defaultValue
-
-// Create some examples jobs
-let jobs =
-    [1..numberOfJobs]
-    |> List.map (fun id -> { 
-        Id = id
-        JobType = randomJobType rng
-        Size = randomJobSize rng 
-    })
-
-// Create some test machines
-let machines =
-    [1..numberOfMachines]
-    |> List.map (fun id -> {
-        Id = id
-        JobTypes = randomJobTypeSet rng
-    })
-
-
-
 #r "nuget: Flips"
 
 open Flips
 open Flips.Types
 open Flips.SliceMap
+
+
+module Types =
+
+    // The Domain
+    [<RequireQualifiedAccess>]
+    type JobType =
+        | A
+        | B
+        | C
+        | D
+
+    type Job = {
+        Id : int
+        JobType : JobType
+        Size : float
+    } with
+        override this.ToString () =
+            $"Job_{this.Id}"
+
+    type Machine = {
+        Id : int
+        JobTypes : Set<JobType>
+    } with
+        override this.ToString () =
+            $"Machine_{this.Id}"
+
+
+module DataGeneration =
+
+    open System
+    open Types
+
+    // Set of JobTypes for iterating over and sampling from
+    let jobTypes = 
+        [|
+            JobType.A
+            JobType.B
+            JobType.C
+            JobType.D
+        |]
+
+    // Some theoretical JobTypeSets to be used in generating
+    // random Machines
+    let jobTypeSets =
+        [|
+            Set jobTypes
+            Set jobTypes.[1..]
+            Set jobTypes.[2..]
+        |]
+
+    let minJobSize = 1
+    let maxJobSize = 3
+
+    let randomJobSize (rng: Random) =
+        rng.Next(minJobSize, maxJobSize)
+        |> float
+
+    let randomJobType (rng: Random) =
+        jobTypes.[rng.Next(0, jobTypes.Length)]
+
+    let randomJobTypeSet (rng: Random) =
+        jobTypeSets.[rng.Next(0, jobTypeSets.Length)]
+
+    let randomJob (rng: Random) (id: int) =
+        { 
+            Id = id
+            JobType = randomJobType rng
+            Size = randomJobSize rng 
+        }
+
+    let randomMachine (rng: Random) (id: int) =
+        {
+            Id = id
+            JobTypes = randomJobTypeSet rng
+        }
+    
+
+open Types
+
+// Setting up parameters for the example
+let rng = System.Random(123)
+let numberOfJobs = 100
+let numberOfMachines = 5
+let maxWorkDifference = 2.0
+
+// Create some examples jobs
+let jobs =
+    [1..numberOfJobs]
+    |> List.map (DataGeneration.randomJob rng)
+
+// Create some test machines
+let machines =
+    [1..numberOfMachines]
+    |> List.map (DataGeneration.randomMachine rng)
+
 
 // A Map from JobType to the Jobs which are of that type
 let jobsForJobType =
@@ -105,6 +113,8 @@ let jobSizes =
     |> List.map (fun job -> job, job.Size)
     |> SMap
 
+
+
 // The Decisions which represent assigning a Job to a Machine
 // The JobType index allows us to slice along the job type
 // which is useful in some of the constraints
@@ -112,7 +122,7 @@ let assignments =
     DecisionBuilder "Assignment" {
         for machine in machines do
         for jobType in machine.JobTypes do
-        for job in Map.tryFindDefault jobType [] jobsForJobType ->
+        for job in jobsForJobType |> Map.tryFind jobType |> Option.defaultValue [] ->
             Boolean
     } |> SMap3
 
@@ -155,7 +165,7 @@ let minWorkConstraints =
 let setups =
     DecisionBuilder "Setups" {
         for machine in machines do
-        for jobType in jobTypes ->
+        for jobType in machine.JobTypes ->
             Boolean
     } |> SMap2
 
@@ -164,8 +174,17 @@ let setups =
 let setupConstraints =
     ConstraintBuilder "SetupRequired" {
         for machine in machines do
-        for jobType in jobTypes ->
+        for jobType in machine.JobTypes ->
             sum (assignments.[machine, jobType, All]) <== (float numberOfJobs) * setups.[machine, jobType]
+    }
+
+// Limit on the amount of JobType D on any given machine
+let maxJobTypeDPercentage = 0.50
+
+let maxJobTypeDConstraints =
+    ConstraintBuilder "MaxTypeD" {
+        for machine in machines ->
+            sum (assignments.[machine, JobType.D, All] .* jobSizes) <== maxJobTypeDPercentage * sum (assignments.[machine, All, All] .* jobSizes)
     }
 
 
@@ -182,11 +201,14 @@ let model =
     |> Model.addConstraint maxWorkDifferenceConstraint
     |> Model.addConstraints setupConstraints
     |> Model.addConstraints jobsAssignmentConstraints
+    |> Model.addConstraints maxJobTypeDConstraints
 
 // Give the solver plenty of time to find a solution
 let settings = { Settings.basic with MaxDuration = 60_000L }
 
 let result = Solver.solve settings model
+
+
 
 match result with
 | Optimal solution ->
@@ -201,26 +223,36 @@ match result with
         |> List.groupBy fst
         |> List.map (fun (machine, jobs) -> machine, jobs |> List.map snd)
 
-    printfn "Assignments:"
-    for (machine, jobs) in machineAssignments do
-        printfn $"Machine: {machine.Id}"
-        for job in jobs do
-            printfn $"\tJob: {job.Id}"
+    // //You can uncomment if you would like to see the individual job assignments
+    // printfn "Assignments:"
+    // for (machine, jobs) in machineAssignments do
+    //     printfn $"Machine: {machine.Id}"
+    //     for job in jobs do
+    //         printfn $"\tJob: {job.Id}"
 
-
-    // Calculate how much each machine is loaded
+    // Calculate the total work for each machine and the amount of job-type D
     let machineLoads =
         machineAssignments
-        |> List.map (fun (machine, jobs) -> machine, jobs |> List.sumBy (fun j -> j.Size))
+        |> List.map (fun (machine, jobs) -> 
+            {| Machine = machine
+               TotalWork = 
+                    jobs 
+                    |> List.sumBy (fun j -> j.Size)
+               JobTypeDWork = 
+                    jobs 
+                    |> List.filter (fun j -> j.JobType = JobType.D) 
+                    |> List.sumBy (fun j -> j.Size)
+            |})
 
     printfn ""
     printfn "Machine Loading:"
-    for (machine, load) in machineLoads do
-        printfn $"Machine: {machine.Id} | Total Load: {load}"
+    for (m) in machineLoads do
+        // Print out the loading for each machine and the percent of job-type D work
+        printfn $"Machine: {m.Machine.Id} | Total Work: {m.TotalWork} | Type D Work %.2f{m.JobTypeDWork / m.TotalWork} %%"
 
     // Find the min and max loads and calculate the difference
     let maxDifference =
-        let loads = machineLoads |> List.map snd
+        let loads = machineLoads |> List.map (fun m -> m.TotalWork)
         (List.max loads) - (List.min loads)
 
     printfn ""
